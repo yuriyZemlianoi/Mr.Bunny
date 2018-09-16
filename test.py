8ed3ad1985e06b1c1d5156fce078537eeb4762eb
ZYuRiY <y.zemlyanoi@gmail.com>
2018-09-16 03:51:02 +0300

    kuna+controller

--- test.py
+++ test.py
@@ -2,3 +2,13 @@
 # result = red.get('0011b149-fefc-4b58-96eb-344a889dc1b7')
 # res = json.loads(result.decode('utf-8'))
 # print(res)
+
+# from kuna.client import KunaAPI
+#
+# clientFirst = KunaAPI('test' 'test')
+# ticker = clientFirst.get_recent_market_data('remuah')['ticker']
+# print(ticker)
+# ticker = clientFirst.get_recent_market_data('btcuah')['ticker']
+# print(ticker)
+# ticker = clientFirst.get_recent_market_data('ethuah')['ticker']
+# print(ticker)
